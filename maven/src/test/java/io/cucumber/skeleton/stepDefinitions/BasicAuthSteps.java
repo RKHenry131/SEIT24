e678fcfff2a4bfb7b3e7f0bb09259b96a85ec9bb
RKHenry131 <rashawn1999@hotmail.com>
2024-10-09 22:26:19 +0100

    Pushing completed Basic Auth Test with implemented BasicAuthSteps

--- maven/src/test/java/io/cucumber/skeleton/stepDefinitions/BasicAuthSteps.java
+++ maven/src/test/java/io/cucumber/skeleton/stepDefinitions/BasicAuthSteps.java
@@ -1,4 +1,27 @@
 package io.cucumber.skeleton.stepDefinitions;
 
-public class BasicAuthSteps {
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.skeleton.core.Context;
+import io.cucumber.skeleton.core.Manager;
+import io.cucumber.skeleton.pages.BasicAuthPage;
+import org.junit.jupiter.api.Assertions;
+
+public class BasicAuthSteps extends Context {
+
+    private BasicAuthPage basicAuthPage = new BasicAuthPage(getDriver());
+
+    public BasicAuthSteps(Manager manager) {
+        super(manager);
+    }
+
+    @And("valid credentials are supplied")
+    public void validCredentialsAreSupplied() {
+        basicAuthPage.login();
+    }
+
+    @Then("Congratulations should be displayed")
+    public void congratulationsShouldBeDisplayed() {
+        Assertions.assertEquals("Congratulations! You must have the proper credentials.", basicAuthPage.getBasicAuthPageText());
+    }
 }
