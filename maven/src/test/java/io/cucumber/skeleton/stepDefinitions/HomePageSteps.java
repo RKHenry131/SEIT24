78dd4eb5772cf3658952d80fbf25e4102c2da40c
RKHenry131 <rashawn1999@hotmail.com>
2024-10-09 21:59:11 +0100

    Pushing completed Home page test, assertions added to step definitions for title display and list of pages

--- maven/src/test/java/io/cucumber/skeleton/stepDefinitions/HomePageSteps.java
+++ maven/src/test/java/io/cucumber/skeleton/stepDefinitions/HomePageSteps.java
@@ -1,17 +1,29 @@
 package io.cucumber.skeleton.stepDefinitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.skeleton.Utils.Utils;
 import io.cucumber.skeleton.core.Context;
 import io.cucumber.skeleton.core.Manager;
+import io.cucumber.skeleton.pages.Home;
+import org.junit.jupiter.api.Assertions;
 
 public class HomePageSteps extends Context {
-
+  private Home home = new Home(getDriver());
   public HomePageSteps(Manager manager) {
     super(manager);
   }
 
-  @Given("an example Home Page step")
+  @Given("I am on the Home Page")
   public void exampleHomePageStep() {
-    System.out.println("printing shared stash :" + getTestStash().toString());
+    Assertions.assertTrue(home.getTitle().isDisplayed());
+    Assertions.assertEquals(Utils.getExpectedHomePageTitle(), home.getTitleText());
   }
+
+  @Then("the displayed list of listed examples is as expected")
+  public void theDisplayedListOfListedExamplesIsAsExpected() {
+    Assertions.assertEquals(Utils.expectedPageList(), home.getListOfPages());
+  }
+
+
 }
