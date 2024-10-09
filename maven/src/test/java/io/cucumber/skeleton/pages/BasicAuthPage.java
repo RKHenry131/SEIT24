22e77666eb11cb013bab10a9d4504ad2bf1458f1
RKHenry131 <rashawn1999@hotmail.com>
2024-10-09 22:19:14 +0100

    Pushing BasicAuthPage implementation

--- maven/src/test/java/io/cucumber/skeleton/pages/BasicAuthPage.java
+++ maven/src/test/java/io/cucumber/skeleton/pages/BasicAuthPage.java
@@ -1,4 +1,28 @@
 package io.cucumber.skeleton.pages;
 
-public class BasicAuthPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class BasicAuthPage extends Page{
+
+    private static final String domain = "the-internet.herokuapp.com/basic_auth";
+    private static final String userName = "admin";
+    private static final String password = "admin";
+
+    @FindBy(xpath = "//*[@id=\"content\"]/div/p")
+    private WebElement expectedPageText;
+
+    public BasicAuthPage(ChromeDriver driver) {
+        super(driver);
+    }
+
+    public void login(){
+        String loginUrl = "https://" + userName + ":" + password + "@" + domain;
+        driver.get(loginUrl);
+    }
+
+    public String getBasicAuthPageText(){
+        return expectedPageText.getText();
+    }
 }
